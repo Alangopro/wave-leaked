dbbe057b88aa90c5f417bb6719e40682a8a66fed
Kamerzystanasyt <91615271+Alangopro@users.noreply.github.com>
2025-05-20 05:29:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,73 +1,18 @@
-/* Toggle Button to Unmute the Video */
-
-function toggleMute() {
-    var video = document.getElementById('video');
-    if (video.muted) {
-        video.muted = false;
-    } else {
-        video.muted = true;
-    }
+// ==== opens the jumpscare ====//
+function showJumpscare() {
+        const jumpscare = document.getElementById("jumpscare");
+        const video = document.getElementById("jumpscareVideo");
+        jumpscare.classList.remove("hidden");
+        video.play();
+        document.body.style.overflow = "hidden";
 }
 
-/* Delay Function to Add SetTimeOut After Defined Interval */
-
-function delay(time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
+// ==== closes the jumpscare ====//
+function closeJumpscare() {
+        const jumpscare = document.getElementById("jumpscare");
+        const video = document.getElementById("jumpscareVideo");
+        jumpscare.classList.add("hidden");
+        video.pause();
+        video.currentTime = 0;
+        document.body.style.overflow = "";
 }
-
-/* Show Video Function to Add Display Property to Show the Video on Click of Button which will fulfilled User Interaction Needs to Browser to Run the Video with Unmute State */
-
-function showVideo() {
-    var element = document.getElementById('video');
-    var button = document.getElementById('container');
-    element.style.display = 'block';
-    button.style.display = 'none';
-    delay(100).then(() => toggleMute());
-}
-
-const fullscreenButton = document.getElementById('button');
-const content = document.getElementById('container-video');
-
-fullscreenButton.addEventListener('click', () => {
-    if (content.requestFullscreen) {
-        content.requestFullscreen();
-    } else if (content.mozRequestFullScreen) { // Firefox
-        content.mozRequestFullScreen();
-    } else if (content.webkitRequestFullscreen) { // Chrome, Safari and Opera
-        content.webkitRequestFullscreen();
-    } else if (content.msRequestFullscreen) { // Internet Explorer/Edge
-        content.msRequestFullscreen();
-    }
-});
-
-document.addEventListener('fullscreenchange', () => {
-    if (document.fullscreenElement) {
-        content.style.display = 'block';
-    } else {
-        content.style.display = 'block';
-    }
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    const counterElement = document.getElementById("counter");
-    const messageElement = document.getElementById("recaptcha-container");
-    const messageElementText = document.getElementById("text-captcha");
-
-    let seconds = 3;
-
-    function updateCounter() {
-        counterElement.textContent = "Please allow up to " + seconds + " seconds...";
-        seconds--;
-
-        if (seconds < 0) {
-            clearInterval(interval);
-            counterElement.style.display = "none";
-            messageElement.style.display = "flex";
-            messageElementText.style.display = "flex"
-        }
-    }
-
-    updateCounter();
-    const interval = setInterval(updateCounter, 1000);
-});
-
